b7a92f03c971db237eb001c5d4c557a4bae7623d
张晨 <zhang_chen@cestc.cn>
2021-06-03 18:19:33 +0800

    Add code.

---
+++ src/go_code/packageProject/leetcode/ContiguousArray.go
@@ -0,0 +1,31 @@
+package main
+
+func main() {
+	println(findMaxLength([]int{0, 1, 0}))
+}
+
+func findMaxLength(nums []int) (maxLength int) {
+	mp := map[int]int{0: -1}
+	counter := 0
+	for i, num := range nums{
+		if num==1{
+			counter++
+		}else {
+			counter--
+		}
+		if prevIndex, has := mp[counter]; has {
+			maxLength = max(maxLength, i-prevIndex)
+		}else {
+			mp[counter] = i
+		}
+
+	}
+	return
+}
+
+func max(a,b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
